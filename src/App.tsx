447ea718031307129e8d0ce4913c033950ca5c47
prokash4433 <prokashdas734@gmail.com>
2026-09-14 17:01:52 +0600

    Added a data json file

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,30 @@
 import { Suspense } from "react";
 import Navbar from "./Components/Navbar";
- 
+
 import Cards from "./Components/ProgrammingCards/Cards";
 import type { Icards } from "./types/cardsTypes";
 import { ToastContainer } from "react-toastify";
- 
- 
+
+
 import Banner from "./Components/Banner";
 import Footer from "./Components/Footer";
- 
- 
- const cardsPromise = async (): Promise<Icards[]>=>{
+
+
+const cardsPromise = async (): Promise<Icards[]> => {
   const res = await fetch("./data.json");
   const data = await res.json();
   return data;
 }
 
 
- 
+
 
 function App() {
   return (
     <>
       <Navbar />
       <Banner />
-       
+
       <ToastContainer />
 
       <Suspense fallback={<h2>Loading...</h2>}>
